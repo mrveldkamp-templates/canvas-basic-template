60fa016180835c505a5fb2ad3a0950e35ec99985
mrveldkamp <colin.veldkamp@epsb.ca>
2022-02-24 09:50:45 -0700

    init template

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
-console.log("hi");
+// Canvas Template
+
+// Set up canvas and graphics context
+let cnv = document.getElementById("my-canvas");
+let ctx = cnv.getContext("2d");
+cnv.width = 800;
+cnv.height = 600;
+
+// Draw Function
+window.addEventListener("load", draw);
+
+function draw() {
+
+  // Request Animation Frame
+  requestAnimationFrame(draw);
+}
